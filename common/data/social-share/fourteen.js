d293d0a88c8baca074706a6371be81f6893ce878
Lauriel Mesquita <laurielmesquita@me.com>
2021-08-17 11:42:52 -0300

    :construction: Keep working...

--- common/data/social-share/fourteen.js
+++ common/data/social-share/fourteen.js
@@ -1,21 +1,15 @@
-import { socialTwitter } from 'react-icons-kit/ionicons/socialTwitter';
-import { socialFacebook } from 'react-icons-kit/ionicons/socialFacebook';
-import { socialGithub } from 'react-icons-kit/ionicons/socialGithub';
+import { socialFacebook } from 'react-icons-kit/ionicons/socialFacebook'
+import { socialInstagram } from 'react-icons-kit/ionicons/socialInstagram'
 
 export const SOCIAL_PROFILES = [
   {
     id: '1',
     icon: socialFacebook,
-    url: '#',
+    url: 'https://www.facebook.com/medplusdistribuidorapi/'
   },
   {
     id: '2',
-    icon: socialTwitter,
-    url: '#',
-  },
-  {
-    id: '3',
-    icon: socialGithub,
-    url: '#',
-  },
-];
+    icon: socialInstagram,
+    url: 'https://www.instagram.com/medplusdistribuidorapi/'
+  }
+]
